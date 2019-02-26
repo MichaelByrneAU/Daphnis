dd4abdb132b2058dce6680ba946666f56c7dc908
Michael Byrne <16238945+MichaelByrneAU@users.noreply.github.com>
2019-02-26 17:05:33 +1100

    Implement chapter 6 contents

---
+++ src/camera.rs
@@ -0,0 +1,27 @@
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new(origin: Vec3, lower_left_corner: Vec3, horizontal: Vec3, vertical: Vec3) -> Camera {
+        Camera {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
+
+mod camera;
 mod hitable;
 mod ray;
 mod sphere;
 mod vec3;
 
+use camera::Camera;
 use hitable::{Hitable, HitableList};
 use ray::Ray;
 use sphere::Sphere;
@@ -11,11 +15,14 @@ use vec3::Vec3;
 fn main() {
     let nx = 200;
     let ny = 100;
+    let ns = 100;
 
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
+
+    let camera = Camera::new(origin, lower_left_corner, horizontal, vertical);
 
     let spheres = vec![
         Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5),
@@ -29,17 +36,22 @@ fn main() {
             .collect(),
     );
 
+    let mut rng = rand::thread_rng();
+
     println!("P3");
     println!("{} {}", nx, ny);
     println!("255");
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = f64::from(i) / f64::from(nx);
-            let v = f64::from(j) / f64::from(ny);
-            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-
-            let col = colour(&r, &world);
+            let mut col = Vec3::new(0.0, 0.0, 0.0);
+            for _ in 0..ns {
+                let u = (f64::from(i) + rng.gen_range(0.0, 1.0)) / f64::from(nx);
+                let v = (f64::from(j) + rng.gen_range(0.0, 1.0)) / f64::from(ny);
+                let r = camera.get_ray(u, v);
+                col += colour(&r, &world);
+            }
+            col /= f64::from(ns);
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
