27bd8acb3f85d5da394220959a47226a959cc3ce
Michael Byrne <16238945+MichaelByrneAU@users.noreply.github.com>
2019-02-26 12:04:16 +1100

    Implement chapter 3 content

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+mod ray;
 mod vec3;
 
+use ray::Ray;
 use vec3::Vec3;
 
 fn main() {
     let nx = 200;
     let ny = 100;
+
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
 
     println!("P3");
     println!("{} {}", nx, ny);
@@ -12,11 +19,10 @@ fn main() {
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new(
-                f64::from(i) / f64::from(nx),
-                f64::from(j) / f64::from(ny),
-                0.2,
-            );
+            let u = f64::from(i) / f64::from(nx);
+            let v = f64::from(j) / f64::from(ny);
+            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let col = colour(&r);
 
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
@@ -25,4 +31,10 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+}
+
+fn colour(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.unit_vector();
+    let t = 0.5 * unit_direction.y() + 1.0;
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
