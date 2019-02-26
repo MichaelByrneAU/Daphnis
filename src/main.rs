9763324a8ba89e8e3339abf049ba129d52e74a6f
Michael Byrne <16238945+MichaelByrneAU@users.noreply.github.com>
2019-02-26 12:16:13 +1100

    Implement chapter 4 content

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,23 @@ fn main() {
     }
 }
 
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+
+    let a = r.direction.dot(r.direction);
+    let b = 2.0 * oc.dot(r.direction);
+    let c = oc.dot(oc) - radius * radius;
+
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn colour(r: &Ray) -> Vec3 {
-    let unit_direction = r.direction.unit_vector();
-    let t = 0.5 * unit_direction.y() + 1.0;
-    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        Vec3::new(1.0, 0.0, 0.0)
+    } else {
+        let unit_direction = r.direction.unit_vector();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+    }
 }
