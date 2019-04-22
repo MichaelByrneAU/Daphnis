67c31085d42c94147b0bc0caf42c7d00fdbdea50
Michael Byrne <16238945+MichaelByrneAU@users.noreply.github.com>
2019-04-23 09:44:09 +1000

    Add testing suite for ray module

--- src/ray.rs
+++ src/ray.rs
@@ -15,3 +15,43 @@ impl Ray {
         self.origin + t * self.direction
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+    // Utility function to check the approximate equality of two vectors.
+    //
+    // Direct equality comparison between two vectors is unavailable due to
+    // the underlying float representation.
+    fn vec_approx_equal(v1: Vector, v2: Vector) {
+        assert_approx_eq!(v1.x, v2.x);
+        assert_approx_eq!(v1.y, v2.y);
+        assert_approx_eq!(v1.z, v2.z);
+    }
+
+    // Construction
+
+    #[test]
+    fn ray_new() {
+        let given = Ray::new(Vector::new(0.0, 1.0, 2.0), Vector::new(2.0, 1.0, 0.0));
+        let expected = Ray {
+            origin: Vector::new(0.0, 1.0, 2.0),
+            direction: Vector::new(2.0, 1.0, 0.0),
+        };
+        vec_approx_equal(given.origin, expected.origin);
+        vec_approx_equal(given.direction, expected.direction);
+    }
+
+    // Methods
+
+    #[test]
+    fn ray_point_at_parameter() {
+        let given = Ray::new(Vector::new(0.0, 1.0, 2.0), Vector::new(1.0, 1.0, 1.0))
+            .point_at_parameter(0.5);
+        let expected = Vector::new(0.5, 1.5, 2.5);
+        vec_approx_equal(given, expected);
+    }
+
+}
